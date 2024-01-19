27f2d4d4108bbbdc84baee05dc687790024a662d
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-01-19 14:11:58 -0800

    login & signup template

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+// import { CanActivateFn } from '@angular/router';
+
+// export const authGuard: CanActivateFn = (route, state) => {
+//   constructor(private authService: AuthService, private router: Router) {}
+
+//   canActivate(): boolean {
+//     const userToken = this.authService.getUserToken();
+
+//     if (userToken) {
+//       // User is authenticated
+//       return true;
+//     } else {
+//       // User is not authenticated, navigate to the login page
+//       this.router.navigate(['login']);
+//       return false;
+//     }
+//   }
+// };
