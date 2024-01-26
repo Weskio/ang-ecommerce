dc4d8470e64782daf6b755b7cf95ae67064387eb
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-01-26 11:50:53 -0800

    checkout init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,13 @@ import { AuthGuard } from './guards/auth.guard';
 import { LandingComponent } from './components/main-page/landing/landing.component';
 import { SingleProductComponent } from './components/main-page/single-product/single-product.component';
 import { AllProductsComponent } from './components/main-page/all-products/all-products.component';
+import { CheckoutComponent } from './components/main-page/checkout/checkout.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'single-product', component: SingleProductComponent },
   { path: 'all-products', component: AllProductsComponent },
+  { path: 'checkout', component: CheckoutComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: RegisterComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
