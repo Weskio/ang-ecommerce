906d1578f7703831b59d3792dc6b60a4d5a19589
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-01-26 16:34:09 -0800

    css modification

--- src/app/components/main-page/all-products/all-products.component.ts
+++ src/app/components/main-page/all-products/all-products.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { ProductService } from '../../../services/product.service';
+import { Product } from '../../../interfaces/product';
+import { NgFor } from '@angular/common';
 
 @Component({
     selector: 'app-all-products',
     standalone: true,
     templateUrl: './all-products.component.html',
     styleUrl: './all-products.component.css',
-    imports: [HeaderComponent, FooterComponent]
+    imports: [HeaderComponent, FooterComponent,NgFor]
 })
 export class AllProductsComponent {
+
+    constructor(
+        private productService: ProductService
+    ) { }
+
+    products: Product[]= this.productService.getProducts();
 
 }
